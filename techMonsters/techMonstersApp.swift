c51af16470d77b47d0ba2c559d67c7f8f8190a88
dilyorbeck <justsharofiddinov@gmail.com>
2024-11-04 10:32:12 +0100

    Initial Commit

---
+++ techMonsters/techMonstersApp.swift
@@ -0,0 +1,17 @@
+//
+//  techMonstersApp.swift
+//  techMonsters
+//
+//  Created by Dilyorbek Sharofiddinov on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct techMonstersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ techMonstersTests/techMonstersTests.swift
@@ -0,0 +1,17 @@
+//
+//  techMonstersTests.swift
+//  techMonstersTests
+//
+//  Created by Dilyorbek Sharofiddinov on 04/11/24.
+//
+
+import Testing
+@testable import techMonsters
+
+struct techMonstersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
